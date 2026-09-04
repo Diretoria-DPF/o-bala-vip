c585d43956a67f488f38b72cc595f7feca53e055
Diretoria-DPF <dpires292@gmail.com>
2026-09-04 03:02:02 -0300

    Update APPS_SCRIPT_URL in api-service.js

--- js/api-service.js
+++ js/api-service.js
@@ -4,7 +4,7 @@
  */
 
 const ApiService = (() => {
-  const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwvoeajXPFtKAyIqFnCtSvlWV7jjz8fQZdZ_0VVPr_RitsAuda79SCSeYHQFKvOTFU/exec';
+  const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyoNhKno4NSdw0aO6dJro2JotqDH8V2rtafkCwtg6zhxAoLtawzq_BPvEBbcxp16_SV/exec';
   const OPENFDA_BASE_URL = 'https://api.fda.gov/drug/label.json';
 
   /**
